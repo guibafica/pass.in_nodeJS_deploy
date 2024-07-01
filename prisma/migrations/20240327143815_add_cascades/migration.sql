22c99c7df37fde453381902accae19abe56738a4
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-07-01 09:08:30 -0300

    Adjusts migrations to fix sintaxe error

--- prisma/migrations/20240327143815_add_cascades/migration.sql
+++ prisma/migrations/20240327143815_add_cascades/migration.sql
@@ -1,7 +1,6 @@
 -- RedefineTables
-PRAGMA foreign_keys=OFF;
 CREATE TABLE "new_check_ins" (
-    "id" serial NOT NULL PRIMARY KEY,
+    "id" SERIAL NOT NULL PRIMARY KEY,
     "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "attendee_id" INTEGER NOT NULL,
     CONSTRAINT "check_ins_attendee_id_fkey" FOREIGN KEY ("attendee_id") REFERENCES "attendees" ("id") ON DELETE CASCADE ON UPDATE CASCADE
@@ -11,7 +10,7 @@ DROP TABLE "check_ins";
 ALTER TABLE "new_check_ins" RENAME TO "check_ins";
 CREATE UNIQUE INDEX "check_ins_attendee_id_key" ON "check_ins"("attendee_id");
 CREATE TABLE "new_attendees" (
-    "id" serial NOT NULL PRIMARY KEY,
+    "id" SERIAL NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -19,8 +18,6 @@ CREATE TABLE "new_attendees" (
     CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 INSERT INTO "new_attendees" ("created_at", "email", "event_id", "id", "name") SELECT "created_at", "email", "event_id", "id", "name" FROM "attendees";
-DROP TABLE "attendees";
+DROP TABLE "attendees" CASCADE;
 ALTER TABLE "new_attendees" RENAME TO "attendees";
 CREATE UNIQUE INDEX "attendees_event_id_email_key" ON "attendees"("event_id", "email");
-PRAGMA foreign_key_check;
-PRAGMA foreign_keys=ON;
